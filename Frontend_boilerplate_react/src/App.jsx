98645b9ffd3a163f0e3d1b68a626119d1915234b
yugandhar0609 <yugandharselvam@gmail.com>
2025-04-16 17:37:31 +0530

    frontend

--- Frontend_boilerplate_react/src/App.jsx
+++ Frontend_boilerplate_react/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './Routes/Index';
 
 const App = () => {
-  console.log("Hello World");
+  console.log("CDT completed");
   return (
     <Router>
       <AppRoutes />
